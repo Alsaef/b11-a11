3dd531dde8ef5a3119366b312e070cc7b2cc2277
Alsaef <saef.ratul@gmail.com>
2025-07-01 18:57:56 +0600

    dev

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,8 @@ import FeatureSection from '../Components/FeatureSection';
 import GallerySection from '../Components/GallerySection';
 import NewsletterSection from '../Components/NewsletterSection';
 import HighLight from '../Components/HighLight';
+import CategoryEvent from '../Components/CategoryEvent';
+import StaticBlog from '../Components/StaticBlog';
 
 const Home = () => {
     return (
@@ -12,7 +14,9 @@ const Home = () => {
             <FeatureSection></FeatureSection>
             <HighLight></HighLight>
             <GallerySection></GallerySection>
+            <CategoryEvent></CategoryEvent>
             <NewsletterSection></NewsletterSection>
+            <StaticBlog></StaticBlog>
         </div>
     );
 };
